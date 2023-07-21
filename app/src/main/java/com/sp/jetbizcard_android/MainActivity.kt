931433004c72ed2f4c647122a218b5f6e96af0bf
Samir Prakash <Samir.Prakash@gmail.com>
2023-07-20 23:50:35 -0500

    added portfolio skeleton

--- app/src/main/java/com/sp/jetbizcard_android/MainActivity.kt
+++ app/src/main/java/com/sp/jetbizcard_android/MainActivity.kt
@@ -7,11 +7,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -39,6 +43,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
+    // mutable state val
+    val buttonClickedState = remember {
+        mutableStateOf(value = false)
+    }
+
     Surface(
         modifier = Modifier
             .fillMaxHeight()
@@ -60,11 +69,50 @@ fun CreateBizCard() {
                 CreateProfilePicture()
                 Divider()
                 CreateInfo()
-                Button(onClick = { Log.d("Clicked", "CreateBizCard: Clicked") }, modifier = Modifier.padding(5.dp)) {
+                Button(
+                    onClick = {
+                        Log.d("Clicked", "CreateBizCard: Clicked")
+                        buttonClickedState.value = !buttonClickedState.value
+                    },
+                    modifier = Modifier.padding(5.dp)
+                ) {
                     Text(text = "View Profile", style = MaterialTheme.typography.button)
+                }
+                if (buttonClickedState.value) {
+                    Content()
+                } else {
+                    Box{}
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(3.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+            Portfolio(data = listOf("Project 1", "Project 2", "Project 3"))
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn {
+        items(data) { item -> Text(item) }
     }
 }
 
@@ -86,7 +134,7 @@ private fun CreateInfo() {
 }
 
 @Composable
-private fun CreateProfilePicture(modifier: Modifier = Modifier) {
+private fun CreateProfilePicture() {
     Surface(
         modifier = Modifier
             .size(150.dp)
